52589221dc11e1ed20f6d359a698d39faf7760b5
Thomas Betts <tbetts42@gmail.com>
2017-11-15 22:20:49 -0700

    Updated form with sample data

--- NotificationsUISampleSoln/NotificationsUISample/Form1.Designer.cs
+++ NotificationsUISampleSoln/NotificationsUISample/Form1.Designer.cs
@@ -36,15 +36,25 @@
             this.typeComboBox = new System.Windows.Forms.ComboBox();
             this._sampleTitlesDataset = new NotificationsUISample.TitlesDataset();
             this.availabilityGroupBox = new System.Windows.Forms.GroupBox();
-            this.seasonPanel = new System.Windows.Forms.Panel();
-            this.seasonComboBox = new System.Windows.Forms.ComboBox();
-            this.seasonLabel = new System.Windows.Forms.Label();
-            this.onServiceLabel = new System.Windows.Forms.Label();
             this.serviceComboBox = new System.Windows.Forms.ComboBox();
+            this.onServiceLabel = new System.Windows.Forms.Label();
+            this.seasonPanel = new System.Windows.Forms.Panel();
+            this.seasonLabel = new System.Windows.Forms.Label();
+            this.seasonComboBox = new System.Windows.Forms.ComboBox();
+            this.eventGroupBox = new System.Windows.Forms.GroupBox();
+            this.eventsComboBox = new System.Windows.Forms.ComboBox();
+            this.comparedToGroupBox = new System.Windows.Forms.GroupBox();
+            this.secondServiceComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.secondTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._sampleTitlesDataset)).BeginInit();
             this.availabilityGroupBox.SuspendLayout();
             this.seasonPanel.SuspendLayout();
+            this.eventGroupBox.SuspendLayout();
+            this.comparedToGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -82,6 +92,7 @@
             this.typeComboBox.Name = "typeComboBox";
             this.typeComboBox.Size = new System.Drawing.Size(305, 33);
             this.typeComboBox.TabIndex = 3;
+            this.typeComboBox.SelectedIndexChanged += new System.EventHandler(this.typeComboBox_SelectedIndexChanged);
             // 
             // _sampleTitlesDataset
             // 
@@ -90,52 +101,19 @@
             // 
             // availabilityGroupBox
             // 
+            this.availabilityGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.availabilityGroupBox.Controls.Add(this.serviceComboBox);
             this.availabilityGroupBox.Controls.Add(this.onServiceLabel);
             this.availabilityGroupBox.Controls.Add(this.label2);
             this.availabilityGroupBox.Controls.Add(this.typeComboBox);
+            this.availabilityGroupBox.Enabled = false;
             this.availabilityGroupBox.Location = new System.Drawing.Point(12, 86);
             this.availabilityGroupBox.Name = "availabilityGroupBox";
             this.availabilityGroupBox.Size = new System.Drawing.Size(912, 128);
-            this.availabilityGroupBox.TabIndex = 4;
+            this.availabilityGroupBox.TabIndex = 3;
             this.availabilityGroupBox.TabStop = false;
-            this.availabilityGroupBox.Visible = false;
-            // 
-            // seasonPanel
-            // 
-            this.seasonPanel.Controls.Add(this.seasonLabel);
-            this.seasonPanel.Controls.Add(this.seasonComboBox);
-            this.seasonPanel.Location = new System.Drawing.Point(516, 12);
-            this.seasonPanel.Name = "seasonPanel";
-            this.seasonPanel.Size = new System.Drawing.Size(408, 68);
-            this.seasonPanel.TabIndex = 5;
-            this.seasonPanel.Visible = false;
-            // 
-            // seasonComboBox
-            // 
-            this.seasonComboBox.FormattingEnabled = true;
-            this.seasonComboBox.Location = new System.Drawing.Point(95, 16);
-            this.seasonComboBox.Name = "seasonComboBox";
-            this.seasonComboBox.Size = new System.Drawing.Size(186, 33);
-            this.seasonComboBox.TabIndex = 0;
-            // 
-            // seasonLabel
-            // 
-            this.seasonLabel.AutoSize = true;
-            this.seasonLabel.Location = new System.Drawing.Point(4, 19);
-            this.seasonLabel.Name = "seasonLabel";
-            this.seasonLabel.Size = new System.Drawing.Size(85, 25);
-            this.seasonLabel.TabIndex = 1;
-            this.seasonLabel.Text = "Season";
-            // 
-            // onServiceLabel
-            // 
-            this.onServiceLabel.AutoSize = true;
-            this.onServiceLabel.Location = new System.Drawing.Point(149, 31);
-            this.onServiceLabel.Name = "onServiceLabel";
-            this.onServiceLabel.Size = new System.Drawing.Size(36, 25);
-            this.onServiceLabel.TabIndex = 4;
-            this.onServiceLabel.Text = "on";
+            this.availabilityGroupBox.Text = "Availability";
             // 
             // serviceComboBox
             // 
@@ -143,13 +121,142 @@
             this.serviceComboBox.Location = new System.Drawing.Point(192, 28);
             this.serviceComboBox.Name = "serviceComboBox";
             this.serviceComboBox.Size = new System.Drawing.Size(305, 33);
-            this.serviceComboBox.TabIndex = 5;
+            this.serviceComboBox.TabIndex = 1;
+            this.serviceComboBox.SelectedIndexChanged += new System.EventHandler(this.serviceComboBox_SelectedIndexChanged);
+            // 
+            // onServiceLabel
+            // 
+            this.onServiceLabel.AutoSize = true;
+            this.onServiceLabel.Location = new System.Drawing.Point(149, 31);
+            this.onServiceLabel.Name = "onServiceLabel";
+            this.onServiceLabel.Size = new System.Drawing.Size(36, 25);
+            this.onServiceLabel.TabIndex = 0;
+            this.onServiceLabel.Text = "on";
+            // 
+            // seasonPanel
+            // 
+            this.seasonPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.seasonPanel.Controls.Add(this.seasonLabel);
+            this.seasonPanel.Controls.Add(this.seasonComboBox);
+            this.seasonPanel.Enabled = false;
+            this.seasonPanel.Location = new System.Drawing.Point(516, 12);
+            this.seasonPanel.Name = "seasonPanel";
+            this.seasonPanel.Size = new System.Drawing.Size(408, 68);
+            this.seasonPanel.TabIndex = 2;
+            // 
+            // seasonLabel
+            // 
+            this.seasonLabel.AutoSize = true;
+            this.seasonLabel.Location = new System.Drawing.Point(4, 19);
+            this.seasonLabel.Name = "seasonLabel";
+            this.seasonLabel.Size = new System.Drawing.Size(85, 25);
+            this.seasonLabel.TabIndex = 0;
+            this.seasonLabel.Text = "Season";
+            // 
+            // seasonComboBox
+            // 
+            this.seasonComboBox.FormattingEnabled = true;
+            this.seasonComboBox.Location = new System.Drawing.Point(95, 16);
+            this.seasonComboBox.Name = "seasonComboBox";
+            this.seasonComboBox.Size = new System.Drawing.Size(186, 33);
+            this.seasonComboBox.TabIndex = 1;
+            this.seasonComboBox.SelectedIndexChanged += new System.EventHandler(this.seasonComboBox_SelectedIndexChanged);
+            // 
+            // eventGroupBox
+            // 
+            this.eventGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.eventGroupBox.Controls.Add(this.eventsComboBox);
+            this.eventGroupBox.Enabled = false;
+            this.eventGroupBox.Location = new System.Drawing.Point(12, 220);
+            this.eventGroupBox.Name = "eventGroupBox";
+            this.eventGroupBox.Size = new System.Drawing.Size(912, 86);
+            this.eventGroupBox.TabIndex = 4;
+            this.eventGroupBox.TabStop = false;
+            this.eventGroupBox.Text = "Event";
+            // 
+            // eventsComboBox
+            // 
+            this.eventsComboBox.FormattingEnabled = true;
+            this.eventsComboBox.Location = new System.Drawing.Point(192, 31);
+            this.eventsComboBox.Name = "eventsComboBox";
+            this.eventsComboBox.Size = new System.Drawing.Size(305, 33);
+            this.eventsComboBox.TabIndex = 0;
+            this.eventsComboBox.SelectedIndexChanged += new System.EventHandler(this.eventsComboBox_SelectedIndexChanged);
+            // 
+            // comparedToGroupBox
+            // 
+            this.comparedToGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comparedToGroupBox.Controls.Add(this.secondServiceComboBox);
+            this.comparedToGroupBox.Controls.Add(this.label3);
+            this.comparedToGroupBox.Controls.Add(this.label4);
+            this.comparedToGroupBox.Controls.Add(this.secondTypeComboBox);
+            this.comparedToGroupBox.Enabled = false;
+            this.comparedToGroupBox.Location = new System.Drawing.Point(12, 312);
+            this.comparedToGroupBox.Name = "comparedToGroupBox";
+            this.comparedToGroupBox.Size = new System.Drawing.Size(912, 128);
+            this.comparedToGroupBox.TabIndex = 5;
+            this.comparedToGroupBox.TabStop = false;
+            this.comparedToGroupBox.Text = "Compared to";
+            // 
+            // secondServiceComboBox
+            // 
+            this.secondServiceComboBox.FormattingEnabled = true;
+            this.secondServiceComboBox.Location = new System.Drawing.Point(192, 28);
+            this.secondServiceComboBox.Name = "secondServiceComboBox";
+            this.secondServiceComboBox.Size = new System.Drawing.Size(305, 33);
+            this.secondServiceComboBox.TabIndex = 1;
+            this.secondServiceComboBox.SelectedIndexChanged += new System.EventHandler(this.secondServiceComboBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "on";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "for";
+            // 
+            // secondTypeComboBox
+            // 
+            this.secondTypeComboBox.FormattingEnabled = true;
+            this.secondTypeComboBox.Location = new System.Drawing.Point(192, 67);
+            this.secondTypeComboBox.Name = "secondTypeComboBox";
+            this.secondTypeComboBox.Size = new System.Drawing.Size(305, 33);
+            this.secondTypeComboBox.TabIndex = 3;
+            this.secondTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.secondTypeComboBox_SelectedIndexChanged);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultTextBox.Location = new System.Drawing.Point(12, 446);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(912, 76);
+            this.resultTextBox.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(936, 580);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.comparedToGroupBox);
+            this.Controls.Add(this.eventGroupBox);
             this.Controls.Add(this.seasonPanel);
             this.Controls.Add(this.availabilityGroupBox);
             this.Controls.Add(this.titlesComboBox);
@@ -162,6 +269,9 @@
             this.availabilityGroupBox.PerformLayout();
             this.seasonPanel.ResumeLayout(false);
             this.seasonPanel.PerformLayout();
+            this.eventGroupBox.ResumeLayout(false);
+            this.comparedToGroupBox.ResumeLayout(false);
+            this.comparedToGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,6 +291,14 @@
         private System.Windows.Forms.ComboBox seasonComboBox;
         private System.Windows.Forms.ComboBox serviceComboBox;
         private System.Windows.Forms.Label onServiceLabel;
+        private System.Windows.Forms.GroupBox eventGroupBox;
+        private System.Windows.Forms.ComboBox eventsComboBox;
+        private System.Windows.Forms.GroupBox comparedToGroupBox;
+        private System.Windows.Forms.ComboBox secondServiceComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox secondTypeComboBox;
+        private System.Windows.Forms.TextBox resultTextBox;
     }
 }
 
--- NotificationsUISampleSoln/NotificationsUISample/Form1.cs
+++ NotificationsUISampleSoln/NotificationsUISample/Form1.cs
@@ -15,6 +15,9 @@ namespace NotificationsUISample
         const string ANY_TITLE = "Any Title";
         const string ANY_SEASON = "Any Season";
         const string ANY_SERVICE = "Any Service";
+        const string ANY_TYPE = "Any Type";
+
+        private string SelectedTitle => titlesComboBox.SelectedItem?.ToString();
 
         public Form1()
         {
@@ -26,6 +29,7 @@ namespace NotificationsUISample
         {
             _sampleTitlesDataset = GetSampleTitlesDataset();
             LoadTitlesDropdown();
+            LoadEvents();
         }
 
         private void LoadTitlesDropdown()
@@ -33,6 +37,29 @@ namespace NotificationsUISample
             titlesComboBox.Items.Clear();
             titlesComboBox.Items.Add(ANY_TITLE);
             titlesComboBox.Items.AddRange(GetDistinctTitles().ToArray());
+        }
+
+        private void LoadEvents()
+        {
+            List<string> events = GetEvents();
+
+            eventsComboBox.Items.Clear();
+            eventsComboBox.Items.AddRange(events.ToArray());
+        }
+
+        private static List<string> GetEvents()
+        {
+            var events = new List<string>
+            {
+                "becomes available",
+                "is no longer available",
+                "availability changes to",
+                "costs more than",
+                "costs less than",
+                "price decreased",
+                "price increased"
+            };
+            return events;
         }
 
         private IEnumerable<string> GetDistinctTitles()
@@ -54,58 +81,183 @@ namespace NotificationsUISample
         private void titlesComboBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             UpdateUIAfterTitleSelected();
+            UpdateResultTextBox();
+        }
+
+        private void UpdateResultTextBox()
+        {
+            resultTextBox.Text = CalculateResultMessage();
+
+        }
+
+        string CalculateResultMessage()
+        {
+            if (string.IsNullOrEmpty(SelectedTitle))
+            {
+                return string.Empty;
+            }
+
+            StringBuilder result = new StringBuilder();
+            result.Append("Notify me when ");
+            result.Append(SelectedTitle);
+
+            result.Append(GetSeasonQualifier());
+
+            // TODO: handle different events
+            result.Append($" {GetEventPhrase()}");
+
+            result.Append($" on {GetServiceQualifier()}");
+
+            return result.ToString();
+        }
+
+        private string GetEventPhrase()
+        {
+            if (eventsComboBox.SelectedItem == null)
+            {
+                return string.Empty;
+            }
+
+            return eventsComboBox.SelectedItem.ToString();
+        }
+
+        private string GetServiceQualifier()
+        {
+            var service = serviceComboBox.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(service))
+            {
+                service = ANY_SERVICE;
+            }
+
+            return service;
+        }
+
+        private string GetSeasonQualifier()
+        {
+            string seasonQualifier;
+            var selectedSeason = seasonComboBox.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(selectedSeason) ||
+                selectedSeason == ANY_SEASON)
+            {
+                seasonQualifier = $", {ANY_SEASON}";
+            }
+            else
+            {
+                seasonQualifier = $", season {selectedSeason}";
+            }
+
+            return seasonQualifier;
         }
 
         private void UpdateUIAfterTitleSelected()
         {
             // We could clear everything, but let's do this one step at a time
             UpdateAndShowSeasonPanel();
-            UpdateAvailabilityGroupBox();
+            UpdateAndShowAvailabilityGroupBox();
+            UpdateAndShowEventGroupBox();
 
         }
 
         void UpdateAndShowSeasonPanel()
         {
             UpdateSeasonComboBox();
-            seasonPanel.Visible = true;
+            seasonPanel.Enabled = true;
         }
 
         void UpdateSeasonComboBox()
         {
             IEnumerable<string> seasons;
-            if (titlesComboBox.SelectedItem.ToString() == ANY_TITLE)
+            if (SelectedTitle== ANY_TITLE)
             {
                 seasons = _sampleTitlesDataset.TitlesTracker.Select(t => t.Season).Distinct();
             }
             else
             {
-                seasons = _sampleTitlesDataset.TitlesTracker.Where(t => t.Title == titlesComboBox.SelectedItem.ToString()).Select(t => t.Season).Distinct();
+                seasons = _sampleTitlesDataset.TitlesTracker.Where(t => t.Title == SelectedTitle).Select(t => t.Season).Distinct();
             }
             seasonComboBox.Items.Clear();
             seasonComboBox.Items.Add(ANY_SEASON);
             seasonComboBox.Items.AddRange(seasons.ToArray());
         }
 
-        void UpdateAvailabilityGroupBox()
+        void UpdateAndShowAvailabilityGroupBox()
         {
             UpdateServiceComboBox();
-            availabilityGroupBox.Visible = true;
+            UpdateTypeComboBox();
+            availabilityGroupBox.Enabled = true;
         }
 
         private void UpdateServiceComboBox()
         {
             IEnumerable<string> services;
-            if (titlesComboBox.SelectedItem.ToString() == ANY_TITLE)
+            if (SelectedTitle== ANY_TITLE)
             {
                 services = _sampleTitlesDataset.TitlesTracker.Select(t => t.Service).Distinct();
             }
             else
             {
-                services = _sampleTitlesDataset.TitlesTracker.Where(t => t.Title == titlesComboBox.SelectedItem.ToString()).Select(t => t.Service).Distinct();
+                services = _sampleTitlesDataset.TitlesTracker.Where(t => t.Title == SelectedTitle).Select(t => t.Service).Distinct();
             }
             serviceComboBox.Items.Clear();
             serviceComboBox.Items.Add(ANY_SERVICE);
             serviceComboBox.Items.AddRange(services.ToArray());
+        }
+
+        private void UpdateTypeComboBox()
+        {
+            IEnumerable<string> type;
+            if (SelectedTitle== ANY_TITLE)
+            {
+                type = _sampleTitlesDataset.TitlesTracker.Select(t => t.Type).Distinct();
+            }
+            else
+            {
+                type = _sampleTitlesDataset.TitlesTracker.Where(t => t.Title == SelectedTitle).Select(t => t.Type).Distinct();
+            }
+            typeComboBox.Items.Clear();
+            typeComboBox.Items.Add(ANY_TYPE);
+            typeComboBox.Items.AddRange(type.ToArray());
+        }
+
+        void UpdateAndShowEventGroupBox()
+        {
+            eventGroupBox.Enabled = true;
+        }
+
+        private void seasonComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateResultTextBox();
+
+        }
+
+        private void serviceComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateResultTextBox();
+
+        }
+
+        private void typeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateResultTextBox();
+
+        }
+
+        private void eventsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateResultTextBox();
+
+        }
+
+        private void secondServiceComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateResultTextBox();
+
+        }
+
+        private void secondTypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateResultTextBox();
+
         }
     }
 }
